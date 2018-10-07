7b835dae10a9eaa65a221426eee3bcfa5ddc08cb
Brecht Baekelandt <brecht.baekelandt@outlook.com>
2018-10-07 17:48:40 +0200

    Added RemoveDiacritics

---
+++ brechtbaekelandt.removeDiacritics/brechtbaekelandt.removeDiactritics/brechtbaekelandt.removeDiactritics/Controllers/Api/DataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using brechtbaekelandt.removeDiactritics.Data.DbContext;
+using brechtbaekelandt.removeDiactritics.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace brechtbaekelandt.removeDiactritics.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly DataDbContext _context;
+
+        public DataController(DataDbContext context)
+        {
+            this._context = context;
+        }
+        
+        [HttpGet]
+        public ActionResult<Collection<Word>> Get([FromQuery]string filter = null)
+        {
+            IOrderedQueryable<Word> query;
+
+            if (filter == null)
+            {
+                query = this._context.Words.OrderBy(w => w.String);
+            }
+            else
+            {
+                query = this._context.Words.Where(w => DataDbContext.RemoveDiacritics(w.String).Contains(DataDbContext.RemoveDiacritics(filter))).OrderBy(w => w.String);
+            }
+            
+            return new Collection<Word>(query.ToArray());
+        }
+    }
+}
---
+++ brechtbaekelandt.removeDiacritics/brechtbaekelandt.removeDiactritics/brechtbaekelandt.removeDiactritics/Data/DbContext/DataDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using brechtbaekelandt.removeDiactritics.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design.Internal;
+
+namespace brechtbaekelandt.removeDiactritics.Data.DbContext
+{
+    public class DataDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Word> Words { get; set; }
+
+        [DbFunction("RemoveDiacritics", "dbo")]
+        public static string RemoveDiacritics(string input)
+        {
+            throw new NotImplementedException("This method can only be called in LINQ-to-Entities!");
+        }
+    }
+}
