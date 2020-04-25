fef0ddab3de0d2c5d9c3987bca5efc0c80ba6c91
Brecht Baekelandt <brecht@avassy.com>
2020-04-25 16:29:21 +0200

    Added brechtbaekelandt.entityFrameworkCoreClassLibrary.

---
+++ brechtbaekelandt.entityFrameworkCoreClassLibrary/Data/Entities/Base.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace brechtbaekelandt.entityFrameworkCoreClassLibrary.Data.Entities
+{
+    public abstract class Base
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ brechtbaekelandt.entityFrameworkCoreClassLibrary/Data/Entities/Country.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace brechtbaekelandt.entityFrameworkCoreClassLibrary.Data.Entities
+{
+    public class Country : Base
+    {
+
+        public Country()
+        {
+            
+        }
+
+        public Country(string isoCode, string name, int id)
+        {
+            this.IsoCode = isoCode;
+            this.Name = name;
+            this.Id = id;
+        }
+
+        public string IsoCode { get; set; }
+
+        public virtual IEnumerable<State> States { get; set; }
+    }
+}
